a9ddee19b7ffcb3cdd3b61e8d2f2f0e0e3b2afc2
zemacedo99 <josedantasmacedo@gmail.com>
2021-11-11 15:35:14 +0000

    2.2 and 2.3

--- src/BigNumber.hs
+++ src/BigNumber.hs
@@ -8,13 +8,16 @@
 -- Portability : POSIX.
 module BigNumber where
 
-import Data.List.Split
-import Data.Text.Internal.Read (digitToInt)
-
 type BigNumber = [Int]
 
+
+-- applying (:"") to the string we convert each Char of the string (string is just a list of Chars) into a single-element list.
+-- next the function read convert the individual strings (Char) into integers.
+-- the map applys that to every char of the string, producing our BigNumber (just a list of Ints)
+
 scanner :: String -> BigNumber
-scanner str = bigNumber
-  where
-    splitString = splitOn "" str
-    bigNumber = [digitToInt (splitString !! n) | n <- [0 .. (length (splitString - 1))]]
+scanner str = map (read . (:"")) str :: BigNumber
+
+output :: BigNumber -> String
+output bn = [head (show str) | str <- bn]
+  
