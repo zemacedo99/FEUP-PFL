cb83694147dc6419234f80a5cae3143ab42c2829
TiagoLimaRocha <tiago.lima.rocha93@gmail.com>
2021-11-11 14:07:13 +0000

    Changed project structure

---
+++ src/lib/modules/BigNumber.hs
@@ -0,0 +1,9 @@
+-- |
+-- Module      : BigNumber
+-- Description : Implementação de uma biblioteca de big-numbers em Haskell
+--               para representar estruturas para big-numbers
+-- Copyright   : (c) PFL G6_07, 2021
+-- License     : GPL-3
+-- Stability   : experimental
+-- Portability : POSIX.
+module BigNumber where
---
+++ src/lib/utils/Utils.hs
@@ -0,0 +1,7 @@
+-- Module      : Utils
+-- Description : Funções de utilidade para biblioteca de big-numbers
+-- Copyright   : (c) PFL G6_07, 2021
+-- License     : GPL-3
+-- Stability   : experimental
+-- Portability : POSIX.
+module Utils where
