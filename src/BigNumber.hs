39cfd82c4e4891440ceb7e6c0c86b74e44af8e8b
TiagoLimaRocha <tiago.lima.rocha93@gmail.com>
2021-11-20 15:45:58 +0000

    fixed output, added todo on addBN

--- src/BigNumber.hs
+++ src/BigNumber.hs
@@ -32,7 +32,9 @@ scanner str
 -- 2.3
 -- output: convert big number into string
 output :: BigNumber -> String
-output bn = [head (show str) | str <- bn]
+output bn
+  | isNegBN bn = "-" ++ show (head bn * (-1)) ++ [head (show digit) | digit <- drop 1 bn]
+  | otherwise = [head (show digit) | digit <- bn]
 
 -- AUX
 minBN :: BigNumber -> BigNumber -> BigNumber
@@ -63,6 +65,9 @@ negBN bn = (head bn * (-1)) : tail bn
 isZeroBN :: BigNumber -> Bool
 isZeroBN bn = sum bn == 0
 
+isNegBN :: BigNumber -> Bool
+isNegBN bn = head bn < 0
+
 -- 2.4
 -- somaBN: sum 2 big numbers
 somaBN' :: Integral t => [t] -> [t] -> t -> [t]
@@ -77,9 +82,10 @@ somaBN' (x : xs) (y : ys) carry = val : somaBN' xs ys res
     val = (x + y + carry) `rem` 10
     res = (x + y + carry) `quot` 10
 
--- TODO acrescentar condições para fazer soma de numeros negativos usando a função subBN
 somaBN :: BigNumber -> BigNumber -> BigNumber
-somaBN bn1 bn2 = reverse (somaBN' (reverse bn1) (reverse bn2) 0)
+somaBN bn1 bn2
+  | isNegBN bn1 || isNegBN bn2 = subBN bn1 bn2
+  | otherwise = reverse (somaBN' (reverse bn1) (reverse bn2) 0)
 
 -- 2.5
 -- subBN: subtract 2 big numbers
@@ -108,8 +114,16 @@ subBN bn1 bn2
 
 -- 2.6
 -- mulBN: multiply 2 big numbers
+mulBN' :: BigNumber -> BigNumber -> BigNumber
+mulBN' bn1 bn2 = rbn1
+  where
+    rbn1 = reverse bn1
+    rbn2 = reverse bn2
+
 mulBN :: BigNumber -> BigNumber -> BigNumber
-mulBN x y = scanner "123"
+mulBN bn1 bn2
+  | isZeroBN bn1 || isZeroBN bn2 = scanner "0"
+  | otherwise = reverse (mulBN' bn1 bn2)
 
 -- 2.7
 -- divBN: divide 2 big numbers
