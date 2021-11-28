abcb150a4d25f70484c17d3aa9e0699360f168ec
TiagoLimaRocha <tiago.lima.rocha93@gmail.com>
2021-11-28 11:53:39 +0000

    fixed fibLista

--- src/Fib.hs
+++ src/Fib.hs
@@ -26,17 +26,17 @@ fibRec n
 -- calculated step by step to get the final state, which is in line with the idea of ​​dynamic programming,
 -- so I think this is a recursive form of dynamic programming in functional programming.
 
-fibLista' :: Num b => (b, b) -> p -> (b, b)
-fibLista' (a, b) _ = (b, a + b)
+fibLista2' :: Num b => (b, b) -> p -> (b, b)
+fibLista2' (a, b) _ = (b, a + b)
 
-fibLista :: Num b => Int -> b
-fibLista n = snd (foldl fibLista' (0, 1) (replicate (n - 1) 0))
+fibLista2 :: Num b => Int -> b
+fibLista2 n = snd (foldl fibLista2' (1, 1) (replicate (n - 1) 0))
 
-fibLista2 :: Num a => Int -> a
-fibLista2 n = fibLista2 !! n
+fibLista :: Num a => Int -> a
+fibLista n = fibLista !! n
   where
-    fibLista2 = 1 : 1 : map f [2 ..]
-    f n = fibLista2 !! (n - 1) + fibLista2 !! (n - 2)
+    fibLista = 1 : 1 : map f [2 ..]
+    f n = fibLista !! (n - 1) + fibLista !! (n - 2)
 
 -- 1.3
 -- Calculates an infinite list of fibonnacci numbers by producing the list of corresponding sums
